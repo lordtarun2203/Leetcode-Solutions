fd46c221ec2dcdce0379f30f9adfc5e7762897a6
Tarun <133484826+lordtarun2203@users.noreply.github.com>
2024-05-27 23:03:44 +0530

    Time: 5 ms (33.11%) | Memory: 10.2 MB (53.24%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int specialArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int l=1,r=100;
-        while(l<=r)
-        {
-            int mid=(l+r)/2;
-            int x=0;
-            for(int i=0;i<nums.size();++i)
-                x+=(nums[i]>=mid);
-            // cout<<mid<<" "<<x<<endl;
-            if(x==mid)
-                return x;
-            if(x>mid)
-                l=mid+1;
-            else
-                r=mid-1;
+        sort(nums.begin(), nums.end());
+
+        int start = 0;
+        int end = nums.size();
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int ans = count(nums, mid);
+
+            if (ans == mid) return mid;
+            else if (ans > mid) start = mid + 1;
+            else end = mid - 1;
         }
+
         return -1;
+    }
+    int count(vector<int>& nums, int target) {
+        int ans = 0;
+        for (int num : nums) {
+            if (num >= target) ans++;
+        }
+        return ans;
     }
 };
